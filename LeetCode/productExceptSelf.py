7ac0ca1129bcbef74e44fe8ac9f02b8fdce5cb75
Wenting <wenttzzheng@gmail.com>
2020-04-15 14:51:32 -0400

    #1 30-Day LeetCoding Challenge Week 2 Q15 solution

--- LeetCode/productExceptSelf.py
+++ LeetCode/productExceptSelf.py
@@ -18,5 +18,25 @@ class Solution(object):
         else:
             output = [prod/n for n in nums]
         return output
-            
+#The following is from LeetCode
+ class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+    # @param {integer[]} nums
+    # @return {integer[]}
+        p = 1
+        n = len(nums)
+        output = []
+        for i in range(0,n):
+            output.append(p)
+            p = p * nums[i]
+        p = 1
+        for i in range(n-1,-1,-1):
+            output[i] = output[i] * p
+            p = p * nums[i]
+        return output
+
         
