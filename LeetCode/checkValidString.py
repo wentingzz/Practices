6a34a7098f8fc0c3a88e9e75d73b9963fea87b05
Wenting <wenttzzheng@gmail.com>
2020-04-16 09:24:59 -0400

    #1 30-Day LeetCoding Challenge Week 3 Q16 solution

--- LeetCode/checkValidString.py
+++ LeetCode/checkValidString.py
@@ -25,3 +25,35 @@ class Solution(object):
             if(idxL.pop() > idxS.pop()):
                 return False
         return True
+
+# Solution from Leetcode
+class Solution(object):
+    def checkValidString(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+        if len(s) == 0:
+            return True
+        
+        low = 0
+        high = 0
+        
+        for s_ in s:
+            
+            if s_ == '(':
+                low += 1
+            else:
+                low -= 1
+            
+            if s_ != ')':
+                high += 1
+            else:
+                high -= 1
+            
+            if high < 0:
+                break
+            low = max(low, 0)
+            
+        return low == 0
