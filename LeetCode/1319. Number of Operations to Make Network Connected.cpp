4b3296baede1936557ff0b8ca800bed181a03cf5
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-08-14 09:33:01 -0700

    Update 1319. Number of Operations to Make Network Connected.cpp #1

--- LeetCode/1319. Number of Operations to Make Network Connected.cpp
+++ LeetCode/1319. Number of Operations to Make Network Connected.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class BetterSolution {
 public:
     int findParent(int n, int parents[]){
         if(parents[n] != n){
@@ -32,5 +32,35 @@ public:
             }
         }
         return res;
+    }
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int m = connections.size();
+        if(m < n-1) return -1;
+        
+        vector<vector<int>> adj(n);
+        for(auto& c: connections){
+            adj[c[0]].push_back(c[1]);
+            adj[c[1]].push_back(c[0]);
+        }
+        vector<bool> seen(n, false);
+        int res = -1;
+        for(int i = 0; i < n; i++){
+            if(!seen[i]) res += dfs(i, adj, seen);
+        }
+        return res;
+    }
+
+    int dfs(int node, vector<vector<int>>& adj, vector<bool>& seen){
+        if(seen[node]) return 0;
+
+        seen[node] = true;
+        for(int next: adj[node]){
+            dfs(next, adj, seen);
+        }
+        return 1;
     }
 };
