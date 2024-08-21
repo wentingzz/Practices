0afa3bba30a7326d2a2df54916051321688d15e0
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-08-21 09:45:57 -0400

    Create 2075. Decode the Slanted Ciphertext.cpp #1
    Basic solution

---
+++ LeetCode/2075. Decode the Slanted Ciphertext.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string decodeCiphertext(string encodedText, int rows) {
+        vector<string> matrix;
+        int n = encodedText.size();
+        int cols = n / rows;
+        for(int i = 0; i < n; i += cols){
+            matrix.push_back(encodedText.substr(i, cols));
+        }
+        string res = "";
+        for(int i = 0; i < cols; i++){
+            for(int j = 0; j < rows && j + i < cols; j++){
+                res += matrix[j][j+i];
+            }
+        }
+
+        res.erase(res.find_last_not_of(' ') + 1);
+        return res;
+    }
+};
+
