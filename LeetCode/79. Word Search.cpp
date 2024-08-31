eb293a88c9d2a62cd6c7b74827e5e669d76680d4
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-08-31 08:23:40 -0400

    Create 79. Word Search.cpp #1

---
+++ LeetCode/79. Word Search.cpp
@@ -0,0 +1,30 @@
+
+class Solution {
+public:
+    int n, m;
+    bool exist(vector<vector<char>>& board, string word) {
+        n = board.size();
+        m = board[0].size();
+        
+        vector<vector<bool>> seen(n, vector<bool>(m, false));
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(board[i][j] == word[0] and dfs(board, seen, i, j, word, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    bool dfs(vector<vector<char>>& board, vector<vector<bool>>& seen, int r, int c, string& word, int idx){
+        if(r >= n  or r < 0 or c >= m or c < 0 or seen[r][c] or board[r][c] != word[idx]) return false;
+
+        if(idx == word.size() - 1) return true;
+
+        seen[r][c] = true;
+
+        bool res = dfs(board, seen, r+1, c, word, idx+1) or dfs(board, seen, r-1, c, word, idx+1) or dfs(board, seen, r, c+1, word, idx+1) or dfs(board, seen, r, c-1, word, idx+1);
+        // cout << "[" << r << "]" << "[" << c <<"] = " << res << endl; 
+        seen[r][c] = false;
+        return res;
+    }
+};
