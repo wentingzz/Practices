da1c59df47a2392fa77a13bd1752c13a85e7d4e3
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-08-31 08:24:08 -0400

    Update 79. Word Search.cpp #1

--- LeetCode/79. Word Search.cpp
+++ LeetCode/79. Word Search.cpp
@@ -1,3 +1,36 @@
+class BetterSolution {
+public:
+    int n, m;
+    bool exist(vector<vector<char>>& board, string word) {
+        n = board.size();
+        m = board[0].size();
+        
+        // vector<vector<bool>> seen(n, vector<bool>(m, false));
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(board[i][j] == word[0] and dfs(board, i, j, word, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    bool dfs(vector<vector<char>>& board, int r, int c, string& word, int idx){
+        if(r >= n  or r < 0 or c >= m or c < 0 or board[r][c] == '-' or board[r][c] != word[idx]) return false;
+
+        if(idx == word.size() - 1) return true;
+
+        char tmp = board[r][c];
+        board[r][c] = '-';
+
+        bool res = dfs(board, r+1, c, word, idx+1) 
+            or dfs(board, r-1, c, word, idx+1) 
+            or dfs(board, r, c+1, word, idx+1) 
+            or dfs(board, r, c-1, word, idx+1);
+        // cout << "[" << r << "]" << "[" << c <<"] = " << res << endl; 
+        board[r][c] = tmp;
+        return res;
+    }
+};
 
 class Solution {
 public:
