498cf276f3a750d4c11c2a4d3be2da45421b765c
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-09-02 11:26:28 -0400

    Update 2961. Double Modular Exponentiation.cpp #1
    refactor to boost performance

--- LeetCode/2961. Double Modular Exponentiation.cpp
+++ LeetCode/2961. Double Modular Exponentiation.cpp
@@ -1,3 +1,20 @@
+class QuickerSolution {
+public:
+    int modPow(int base, int power, int mod){
+        int res = 1;
+        for(int i = 0; i < power; i++) res = (res * base) % mod;
+        return res;
+    }
+    vector<int> getGoodIndices(vector<vector<int>>& variables, int target) {
+        int n = variables.size();
+        vector<int> res;
+        for(int i = 0; i < n; i++){
+            if(modPow(modPow(variables[i][0], variables[i][1], 10), variables[i][2], variables[i][3]) == target) res.push_back(i);
+        }
+        return res;
+    }
+};
+
 class Solution {
 public:
     int modPow(int base, int power, int mod){
