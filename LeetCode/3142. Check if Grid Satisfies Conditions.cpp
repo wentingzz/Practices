439717a87f722eb9e1848a3fd3e13196d3bf3792
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-09-12 11:07:26 -0700

    Create 3142. Check if Grid Satisfies Conditions.cpp #1

---
+++ LeetCode/3142. Check if Grid Satisfies Conditions.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool satisfiesConditions(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(i < n - 1 and grid[i][j] != grid[i+1][j]) return false;
+                if(j < m - 1 and grid[i][j] == grid[i][j+1]) return false;
+            }
+        }
+        return true;
+    }
+};
