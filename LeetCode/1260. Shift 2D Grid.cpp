2aaa575adf00ee2f0799e597f0512e96f2304738
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-09-19 08:24:33 -0400

    Update 1260. Shift 2D Grid.cpp #1

--- LeetCode/1260. Shift 2D Grid.cpp
+++ LeetCode/1260. Shift 2D Grid.cpp
@@ -1,4 +1,21 @@
-class Solution {
+class BetterTimeSolution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int n = grid.size(), m = grid[0].size();
+        k = k % (m * n);
+        vector<vector<int>> res(n, vector<int>(m));
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                int tmp = j + k;
+                res[(i + (tmp)/m) % n][tmp % m] = grid[i][j];
+            }
+        }
+        return res;
+    }
+};
+
+class SlightlyBetterSpaceSolution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
         int m = grid.size(), n = grid[0].size();
