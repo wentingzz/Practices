1185c13ccd41b6e32cc38c05627c85da951317ec
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-09-20 09:44:35 -0400

    Update 241. Different Ways to Add Parentheses.cpp #1

--- LeetCode/241. Different Ways to Add Parentheses.cpp
+++ LeetCode/241. Different Ways to Add Parentheses.cpp
@@ -1,3 +1,30 @@
+class BetterSolution {
+public:
+    unordered_map<string, vector<int>> dp;
+    vector<int> diffWaysToCompute(string expression) {
+        if(!dp[expression].empty()) return dp[expression];
+        int n = expression.size();
+
+        vector<int> res;
+        for(int i = 0; i < n; i++){
+            char c = expression[i];
+            if(c == '+' or c == '-' or c == '*'){
+                vector<int> left = diffWaysToCompute(expression.substr(0,i));
+                vector<int> right = diffWaysToCompute(expression.substr(i+1));
+                for(int& l: left){
+                    for(int& r: right){
+                        if(c == '+') res.push_back(l + r);
+                        if(c == '-') res.push_back(l - r);
+                        if(c == '*') res.push_back(l * r);
+                    }
+                }
+            }
+        }
+        if(res.empty()) res.push_back(stoi(expression));
+        return dp[expression] = res;
+    }
+};
+
 class Solution {
 public:
     void evalExp(vector<int>& ls, vector<int>& rs, char& op, vector<int>& res){
