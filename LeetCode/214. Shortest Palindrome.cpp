e0d400990b1f73029d02eefe5cfcdf8ba7723941
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-09-20 09:41:30 -0400

    Update 214. Shortest Palindrome.cpp #1

--- LeetCode/214. Shortest Palindrome.cpp
+++ LeetCode/214. Shortest Palindrome.cpp
@@ -31,3 +31,34 @@ public:
         return s;
     }
 };
+
+// the following code is copied form https://leetcode.com/problems/shortest-palindrome/submissions/1396477603/?envType=daily-question&envId=2024-09-20
+class TimeEffectiveSolution {
+public:
+    string shortestPalindrome(string s) {
+        int count = kmp(string(s.rbegin(), s.rend()), s);
+        return string(s.rbegin(), s.rend()).substr(0, s.length() - count) + s;
+    }
+
+private:
+    int kmp(const string &txt, const string &patt) {
+        string newString = patt + '#' + txt;
+        vector<int> pi(newString.length(), 0);
+        int i = 1, k = 0;
+        while (i < newString.length()) {
+            if (newString[i] == newString[k]) {
+                k++;
+                pi[i] = k;
+                i++;
+            } else {
+                if (k > 0) {
+                    k = pi[k - 1];
+                } else {
+                    pi[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return pi.back();
+    }
+};
