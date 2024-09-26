5e517b9cd7b328d1cee5863f2b78f3508c4702b0
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-09-26 08:42:29 -0400

    Update 729. My Calendar I.cpp #1

--- LeetCode/729. My Calendar I.cpp
+++ LeetCode/729. My Calendar I.cpp
@@ -13,3 +13,29 @@ public:
     }
 };
 
+// Node solution
+struct Node{
+    int start, end;
+    Node *left = NULL, *right = NULL;
+    Node(int s, int e): start(s), end(e){};
+};
+class NodeMyCalendar {
+public:
+    Node* root = NULL;
+    MyCalendar() {
+    }
+    bool book(int start, int end) {
+        return book(start, end, root); // Pass the root explicitly
+    }
+
+private:
+    bool book(int start, int end, Node* &cur) {
+        if(!cur){
+            cur = new Node(start, end);
+            return true;
+        }
+        if(start >= cur->end) return book(start, end, cur->right);
+        if(end <= cur->start) return book(start, end, cur->left);
+        return false;
+    }
+};
