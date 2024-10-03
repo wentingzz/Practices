7f254366108330fb03c1ab941d1a0cc1d784eb2c
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-10-03 17:49:33 -0400

    Update 1331. Rank Transform of an Array.cpp #1

--- LeetCode/1331. Rank Transform of an Array.cpp
+++ LeetCode/1331. Rank Transform of an Array.cpp
@@ -1,3 +1,21 @@
+class BetterSolution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        if(arr.empty()) return arr;
+        vector<pair<int, int>> tmp;
+        for(int i = 0; i < arr.size(); i++) tmp.push_back({arr[i], i});
+        sort(tmp.begin(), tmp.end());
+        int last = tmp[0].first, rank = 1;
+        for(auto it: tmp){
+            if(it.first > last){
+                last = it.first, arr[it.second] = ++rank;
+            }else arr[it.second] = rank;
+        }
+
+        return arr;
+    }
+};
+
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
