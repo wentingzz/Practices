980ff9ca6448ab217105e749a0cf26f509694401
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-10-04 10:08:10 -0400

    Update 2491. Divide Players Into Teams of Equal Skill.cpp #1

--- LeetCode/2491. Divide Players Into Teams of Equal Skill.cpp
+++ LeetCode/2491. Divide Players Into Teams of Equal Skill.cpp
@@ -1,3 +1,24 @@
+class BetterSolution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+        long long res = 0;
+        int total = 0, n = skill.size()/2;
+        vector<int> freq(1001, 0);
+        for(int& n: skill) total += n, freq[n]++;
+        if(total % n > 0) return -1;
+        total /= n;
+        for(int i = 1; i <= min(total, 1000); i++){
+            if(freq[i] == 0 or total - i < 1 or total - i > 1000) continue;
+            if(freq[i] != freq[total - i]) return -1;
+            if(i == total - i) res += (long long)i * (total - i) * freq[i]/2;
+            else res += (long long) i * (total - i) * freq[i];
+            freq[i] = 0, freq[total - i] = 0;
+        }
+        
+        return res;
+    }
+};
+
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
