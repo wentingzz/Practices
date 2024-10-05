28e15303fbd456057aabf420377eee11ce4508b6
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-10-05 09:01:17 -0400

    Update 724. Find Pivot Index.cpp #1

--- LeetCode/724. Find Pivot Index.cpp
+++ LeetCode/724. Find Pivot Index.cpp
@@ -1,3 +1,17 @@
+class QuickerSolution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total = 0, sum = 0;
+        for(int& n: nums) total += n;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(sum == total - sum - nums[i]) return i;
+            sum += nums[i];
+        }
+        return -1;
+    }
+};
+
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
