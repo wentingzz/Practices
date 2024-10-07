ad662d7238a4ef6e9ab5a9db82e7012699e0cb9f
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-10-07 18:01:48 -0400

    Update 23. Merge k Sorted Lists.cpp #1

--- LeetCode/23. Merge k Sorted Lists.cpp
+++ LeetCode/23. Merge k Sorted Lists.cpp
@@ -8,7 +8,43 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+struct CompareListNode{
+    bool operator()(ListNode* l1, ListNode* l2){
+        if(l1 and l2) return l1->val > l2->val;
+        if(l1) return true;
+        return false;
+    }
+};
+class TimeSolution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, CompareListNode> pq;
+        for(ListNode* l: lists){
+            if(l) pq.push(l);
+        }
+        ListNode * res = new ListNode(0), *cur = res;
+        
+        while(!pq.empty()){
+            cur->next = pq.top(); pq.pop();
+            cur = cur->next;
+            if(cur->next) pq.push(cur->next);
+        }
+        return res->next;
+    }
+};
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class SpaceSolution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         int n = lists.size();
