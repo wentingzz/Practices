949b015df03b4a185436b5b831f540ec031c2828
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-10-08 11:10:55 -0400

    Create 238. Product of Array Except Self.cpp #1

---
+++ LeetCode/238. Product of Array Except Self.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n, 1);
+        int prod = 1;
+        for(int i = 0; i < n; i++){
+            res[i] *= prod;
+            prod *= nums[i];
+        }
+        cout << endl;
+        prod = 1;
+        for(int i = n-1; i > -1; i--){
+            res[i] *= prod;
+            prod *= nums[i];
+        }
+        return res;
+    }
+};
