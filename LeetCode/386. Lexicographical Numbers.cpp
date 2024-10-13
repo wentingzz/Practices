02aa5284a47bbe19f281e7d282151ba632b55d83
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-10-13 10:18:38 -0400

    Update 386. Lexicographical Numbers.cpp #1
    #5 30%Time

--- LeetCode/386. Lexicographical Numbers.cpp
+++ LeetCode/386. Lexicographical Numbers.cpp
@@ -12,3 +12,26 @@ public:
         return res;
     }
 };
+
+class SpaceBestSolution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> res(n, 0);
+        int idx = 0;
+        for(int i = 0; i < 10; i++){
+            dfs(i+1, idx, n, res);
+        }
+        return res;
+    }
+
+    void dfs(int cur, int& idx, int n, vector<int>& res){
+        if(idx >= n or cur > n) return;
+        res[idx++] = cur;
+        cur *= 10;
+
+
+        for(int i = 0; i < 10; i++){
+            dfs(cur + i, idx, n, res);
+        }
+    }
+};
