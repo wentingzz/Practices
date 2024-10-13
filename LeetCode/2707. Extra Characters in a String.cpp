7c5ce49e05a42633d711b341542155e81c6dd896
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-10-13 10:03:26 -0400

    Update 2707. Extra Characters in a String.cpp #1

--- LeetCode/2707. Extra Characters in a String.cpp
+++ LeetCode/2707. Extra Characters in a String.cpp
@@ -19,3 +19,27 @@ public:
         return dp[n];
     }
 };
+
+
+class SlowerSolution {
+public:
+    bool startsWithAtIndex(const string& s, const string& x, size_t i) {
+        if (i + x.size() > s.size())  return false;
+        return s.substr(i, x.size()) == x;
+    }
+
+    unordered_map<int, int> dp;
+    int minExtraChar(string s, vector<string>& dictionary, int idx = 0) {
+        if(dp.find(idx) != dp.end()) return dp[idx];
+        if(idx == s.size()) return 0;
+        dp[idx] = minExtraChar(s, dictionary, idx + 1) + 1;
+        
+        for(string& d: dictionary){
+            if(startsWithAtIndex(s, d, idx)){
+                dp[idx] = min(dp[idx], minExtraChar(s, dictionary, idx + d.size()));
+            }
+        }
+
+        return dp[idx];
+    }
+};
