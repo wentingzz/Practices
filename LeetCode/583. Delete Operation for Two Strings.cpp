d718aa011566d10345ab6b993d813f142daf5fcf
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-10-14 08:11:19 -0400

    Create 583. Delete Operation for Two Strings.cpp #1

---
+++ LeetCode/583. Delete Operation for Two Strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size(), m = word2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, m+n));
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] - 2;
+                }else{
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
