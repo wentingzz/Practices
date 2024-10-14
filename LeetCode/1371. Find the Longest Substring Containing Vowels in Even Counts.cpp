1e44eacdbbe4f22f9983c3b5e2d46f403b27a3b6
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-10-14 10:48:27 -0400

    Update 1371. Find the Longest Substring Containing Vowels in Even Counts.cpp #1

--- LeetCode/1371. Find the Longest Substring Containing Vowels in Even Counts.cpp
+++ LeetCode/1371. Find the Longest Substring Containing Vowels in Even Counts.cpp
@@ -1,3 +1,29 @@
+class QuickerSolution {
+public:
+    int findTheLongestSubstring(string s) {
+        int n = s.size(), res = 0;
+        vector<int> preSum(n, 0);
+        vector<int> first(32, -2);
+        first[0] = -1;
+        vector<int> v2i(26, -1);
+        v2i['a' - 'a'] = 0;
+        v2i['e' - 'a'] = 1;
+        v2i['i' - 'a'] = 2;
+        v2i['o' - 'a'] = 3;
+        v2i['u' - 'a'] = 4;
+
+        int cur = 0;
+        for(int i = 0; i < n; i++){
+            if(v2i[s[i]-'a'] != -1) cur ^= 1 << (v2i[s[i]-'a']);
+            preSum[i] = cur;
+            if(first[cur] == -2) first[cur] = i;
+            else res = max(res, i - first[cur]);
+            
+        }
+        return res;
+    }
+};
+
 class SlowerSolution {
 public:
     int findTheLongestSubstring(string s) {
