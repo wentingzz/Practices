c4b41256b9e459092eef60f4ffe04332dbc4bf4d
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-10-15 11:42:00 -0400

    Update 2404. Most Frequent Even Element.cpp #1

--- LeetCode/2404. Most Frequent Even Element.cpp
+++ LeetCode/2404. Most Frequent Even Element.cpp
@@ -1,3 +1,20 @@
+class SlightlyBetterSolution {
+public:
+    int mostFrequentEven(vector<int>& nums) {
+        unordered_map<int, int> m;
+        int count = 0, res = INT_MAX;
+        for(int& n: nums){
+            if(n % 2) continue;
+            m[n]++;
+            count = max(count, m[n]);
+        }
+        for(auto& it: m){
+            if(it.second == count) res = min(res, it.first);
+        }
+        return res == INT_MAX ? -1 : res;
+    }
+};
+
 class Solution {
 public:
     int mostFrequentEven(vector<int>& nums) {
