4f7d2514ec60ac35afba018a6093fb26aa2b90a5
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-10-16 15:50:29 -0400

    Update 1894. Find the Student that Will Replace the Chalk.cpp #1

--- LeetCode/1894. Find the Student that Will Replace the Chalk.cpp
+++ LeetCode/1894. Find the Student that Will Replace the Chalk.cpp
@@ -1,15 +1,39 @@
+class BetterSolution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        long total = chalk[0];
+        for(int i = 1; i < chalk.size(); i++) total += chalk[i];
+        k = (k % total);
+        //binary search can be more effective
+        for(int i = 0; i < chalk.size(); i++){
+            if(chalk[i] > k) return i;
+            k -= chalk[i];
+        }
+        return -1;
+    }
+};
+
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        long total = chalk[0], n = chalk.size();
-        for(int i = 1; i < n; i++) total += chalk[i];
-        k = (k % total);
-        //binary search can be more effective
-        total = 0;
+        long total = 0, n = chalk.size();
+        vector<long> sum(n);
         for(int i = 0; i < n; i++){
             total += chalk[i];
-            if(total > k) return i;
+            sum[i] = total;
         }
-        return -1;
+        k = (k % total);
+        //binary search i where sum[i] > target and i is the smallest
+        int l = 0, r = n, m, res = -1;
+        while(l <= r){
+            m = l + (r-l)/2;
+            if(sum[m] > k){
+                res = m;
+                r = m - 1;
+            }else{
+                l = m + 1;
+            }
+        }
+        return res;
     }
 };
