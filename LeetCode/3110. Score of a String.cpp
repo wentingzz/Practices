3368e891d853624f5e6db3b15c7121f9301fc487
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-10-17 15:27:45 -0400

    Create 3110. Score of a String.cpp #1

---
+++ LeetCode/3110. Score of a String.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int res = 0;
+        for(int i = 1; i < s.size(); i++) res += abs(s[i]-s[i-1]);
+        return res;
+    }
+};
