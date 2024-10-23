1e207c896d124553532ebcea80b17d43cfbca4f1
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-10-23 18:54:15 -0400

    Update 2641. Cousins in Binary Tree II.cpp #1

--- LeetCode/2641. Cousins in Binary Tree II.cpp
+++ LeetCode/2641. Cousins in Binary Tree II.cpp
@@ -9,6 +9,33 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class QuickerSolution {
+public:
+    vector<int> sums;
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        levelSum(root, 0);
+        replace(root, 0, 0);
+        return root;
+    }
+
+    void levelSum(TreeNode* node, int lvl){
+        if(!node) return;
+        if(lvl == sums.size()) sums.push_back(node->val);
+        else sums[lvl] += node->val;
+        levelSum(node->left, lvl+1);
+        levelSum(node->right, lvl+1);
+    }
+
+    void replace(TreeNode* node, int sibling, int lvl){
+        if(!node) return;
+        node->val = sums[lvl] - node->val - sibling;
+        int l = (node->left ? node->left-> val : 0);
+        int r = (node->right ? node->right-> val : 0);
+        replace(node->left, r, lvl+1);
+        replace(node->right, l, lvl+1);
+    }
+};
+
 class Solution {
 public:
     
