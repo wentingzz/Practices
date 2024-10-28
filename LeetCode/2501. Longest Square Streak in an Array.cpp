8c66ac2b431b4a653d32724d5bbd720ca8a95310
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-10-28 09:59:08 -0400

    Update 2501. Longest Square Streak in an Array.cpp #1

--- LeetCode/2501. Longest Square Streak in an Array.cpp
+++ LeetCode/2501. Longest Square Streak in an Array.cpp
@@ -1,7 +1,6 @@
 class TimeSolution {
 public:
     int longestSquareStreak(vector<int>& nums) {
-        // bitset<100001> seen;
         sort(nums.begin(), nums.end());
         vector<int> dp(100001, -1);
         int res = -1;
@@ -12,7 +11,25 @@ public:
                 dp[n] = dp[rt] + 1;
                 res = max(res, dp[n]);
             }
-            // cout << dp[n] << " ";
+        }
+        return res;
+        
+    }
+};
+
+class SpaceSolution {
+public:
+    int longestSquareStreak(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        unordered_map<int, int> dp;
+        int res = -1;
+        for(int& n: nums){
+            dp[n] = 1;
+            int rt = sqrt(n);
+            if(rt * rt == n and dp.find(rt) != dp.end()){
+                dp[n] = dp[rt] + 1;
+                res = max(res, dp[n]);
+            }
         }
         return res;
         
