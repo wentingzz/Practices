25eb209377dc625bdf6ce9f0b564281ad2c65fd1
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-10-30 12:59:03 -0400

    Update 1671. Minimum Number of Removals to Make Mountain Array.cpp #1

--- LeetCode/1671. Minimum Number of Removals to Make Mountain Array.cpp
+++ LeetCode/1671. Minimum Number of Removals to Make Mountain Array.cpp
@@ -1,3 +1,31 @@
+class TimeSolution {
+public:
+    int n;
+    vector<int> LIS(vector<int>& nums){
+        vector<int> res(n, 1);
+        vector<int> tmp;
+        for(int i = 0; i < n; i++){
+            auto pos = lower_bound(tmp.begin(), tmp.end(), nums[i]);
+            if(pos == tmp.end()) tmp.push_back(nums[i]);
+            else *pos = nums[i];
+            res[i] = tmp.size();
+        }
+        return res;
+    }
+    int minimumMountainRemovals(vector<int>& nums) {
+        n = nums.size();
+        vector<int> up = LIS(nums);
+        reverse(nums.begin(), nums.end());
+        vector<int> down = LIS(nums);
+        reverse(down.begin(), down.end());
+        int res = 0;
+        for(int i = 1; i < n - 1; i++){
+            if(up[i] > 1 and down[i] > 1) res = max(res, up[i] + down[i]);
+        }
+        return n - res+1;
+    }
+};
+
 class SpaceSolution {
 public:
     int n;
