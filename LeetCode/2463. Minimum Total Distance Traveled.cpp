7e26115270270940035e5d9ce00974c75a0e2781
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-10-31 20:10:05 -0400

    Update 2463. Minimum Total Distance Traveled.cpp #1

--- LeetCode/2463. Minimum Total Distance Traveled.cpp
+++ LeetCode/2463. Minimum Total Distance Traveled.cpp
@@ -23,3 +23,27 @@ public:
         return dp[0][0];
     }
 };
+
+class BetterSolution {
+public:
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+        sort(robot.begin(), robot.end());
+        sort(factory.begin(), factory.end());
+        vector<int> oneFac;
+        for(auto& f: factory){
+            for(int i = 0; i < f[1]; i++) oneFac.push_back(f[0]);
+        }
+        int n = robot.size(), m = oneFac.size();
+        vector<vector<long long>> dp(n+1, vector<long long>(m+1, 1000000000000));
+        for(int i = 0; i <= m; i++) dp[0][i] = 0;
+        // rec(robot, oneFac, 0, 0, dp);
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                dp[i][j] = min(dp[i-1][j-1] + abs(robot[i-1] - oneFac[j-1]), dp[i][j-1]);
+            }
+        }
+        long long res = dp[n][m];
+        for(int i = 0; i < m; i++) res = min(res, dp[n][i]);
+        return res;
+    }
+};
