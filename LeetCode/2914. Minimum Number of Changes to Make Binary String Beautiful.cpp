672d1b1bd05308ae34c9ae313a642593f336fd0b
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-11-05 09:13:13 -0500

    Create 2914. Minimum Number of Changes to Make Binary String Beautiful.cpp #1

---
+++ LeetCode/2914. Minimum Number of Changes to Make Binary String Beautiful.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minChanges(string s) {
+        int res = 0;
+        for(int i = 1; i < s.size(); i += 2){
+            res += (s[i] != s[i-1]);
+        }
+        return res;
+    }
+};
