27f44bfded823ff40808f93d064f44e576b5179e
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-11-08 07:45:54 -0500

    Update 1829. Maximum XOR for Each Query.cpp

--- LeetCode/1829. Maximum XOR for Each Query.cpp
+++ LeetCode/1829. Maximum XOR for Each Query.cpp
@@ -1,3 +1,19 @@
+class BetterSolution {
+public:
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        int n = nums.size();
+        vector<int> prev(n+1);
+        prev[0] = pow(2, maximumBit) - 1;
+        for(int i = 0; i < n; i++){
+            prev[i+1] = prev[i] ^ nums[i];
+        }
+        reverse(prev.begin(), prev.end());
+        prev.pop_back();
+        return prev;
+
+    }
+};
+
 class Solution {
 public:
     vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
