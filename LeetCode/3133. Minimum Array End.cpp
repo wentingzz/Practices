ac8ad252cebe4790b797d87dc1d4d8305473ab37
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-11-09 09:17:46 -0500

    Update 3133. Minimum Array End.cpp #1

--- LeetCode/3133. Minimum Array End.cpp
+++ LeetCode/3133. Minimum Array End.cpp
@@ -13,3 +13,23 @@ public:
         return res;
     }
 };
+
+class SimilarSolution {
+public:
+    long long minEnd(int n, int x) { 
+        // 2 = 10 (replace all 0s to n bits in order)
+        // x (100) => (1)10
+        // 14 = 1110
+        // x (100) => 11(1)10
+        bitset<64> res(x);
+        bitset<64> cnt(n-1);
+        int bit = 0;
+        for(int i = 0; i < 64; i++){
+            if(res[i] == 0){
+                res[i] = cnt[bit];
+                bit++;
+            }
+        }
+        return res.to_ullong();
+    }
+};
