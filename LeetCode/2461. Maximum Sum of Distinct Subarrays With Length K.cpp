882567f9b71c099ccbd184d592b20e60db9849e7
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-11-19 08:46:04 -0500

    Update 2461. Maximum Sum of Distinct Subarrays With Length K.cpp #1

--- LeetCode/2461. Maximum Sum of Distinct Subarrays With Length K.cpp
+++ LeetCode/2461. Maximum Sum of Distinct Subarrays With Length K.cpp
@@ -1,3 +1,28 @@
+class BetterSolution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        long long res = 0, cur = 0;
+        int freq[100001] = {};
+        int cnt = 0;
+        for(int i = 0; i < k; i++){
+            if(freq[nums[i]] == 1) cnt++; //count non-distinct elements
+            cur += nums[i];
+            freq[nums[i]]++;
+        }
+        if(cnt == 0) res = max(res, cur);
+        for(int i = k; i < nums.size(); i++){
+            if(freq[nums[i]] == 1) cnt++;
+            freq[nums[i]]++;
+            if(freq[nums[i-k]] == 2) cnt--; 
+            freq[nums[i-k]]--;
+            cur += nums[i] - nums[i-k];
+
+            if(cnt == 0) res = max(res, cur);
+        }
+        return res;
+        
+    }
+};
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
