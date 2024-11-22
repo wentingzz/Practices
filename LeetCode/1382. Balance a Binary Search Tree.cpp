f5ce8fd45f13040ce4568083d01bd206beba792d
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-11-22 08:27:15 -0500

    Update 1382. Balance a Binary Search Tree.cpp #1

--- LeetCode/1382. Balance a Binary Search Tree.cpp
+++ LeetCode/1382. Balance a Binary Search Tree.cpp
@@ -35,3 +35,42 @@ public:
         inOrder(root->right);
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BetterSolution {
+public:
+    vector<TreeNode*> nodes;
+    int n;
+    TreeNode* balanceBST(TreeNode* root) {
+        inOrder(root);
+        n = nodes.size();
+        return construct(0, n-1);
+    }
+
+    TreeNode* construct(int l, int r){
+        if(l > r) return nullptr;
+        int m = l + (r-l)/2;
+        TreeNode* node = nodes[m];
+        node->left = construct(l, m-1);
+        node->right = construct(m+1, r);
+        return node;
+    }
+
+    void inOrder(TreeNode* root){
+        if(!root) return;
+        inOrder(root->left);
+        nodes.push_back(root);
+        inOrder(root->right);
+    }
+};
