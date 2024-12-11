79f3d78571af660c5ae0bcb74c7ad8e9b7588c1c
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-12-11 10:54:04 -0500

    Update 690. Employee Importance.cpp #1

--- LeetCode/690. Employee Importance.cpp
+++ LeetCode/690. Employee Importance.cpp
@@ -28,3 +28,31 @@ public:
         return res;
     }
 };
+
+/*
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+*/
+
+class Solution {
+public:
+    int getImportance(vector<Employee*> employees, int id) {
+        unordered_map<int, Employee*> id2e;
+        for(int i = 0; i < employees.size(); i++) id2e[employees[i]->id] = employees[i];
+
+        int res = 0;
+        queue<Employee*> q;
+        q.push(id2e[id]);
+        while(!q.empty()){
+            Employee* e = q.front(); q.pop();
+            res += e->importance;
+            for(int& sub: e->subordinates) q.push(id2e[sub]);
+        }
+        return res;
+    }
+};
