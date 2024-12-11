31d5574d51cb24cf7f2600b9707b5aefd491ac9b
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-12-11 08:44:03 -0500

    Update 2779. Maximum Beauty of an Array After Applying Operation.cpp #1

--- LeetCode/2779. Maximum Beauty of an Array After Applying Operation.cpp
+++ LeetCode/2779. Maximum Beauty of an Array After Applying Operation.cpp
@@ -1,3 +1,22 @@
+class LessTimeSolution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        int freq[100001] {};
+        int l = INT_MAX, h = 0;
+        for(int& n: nums){
+            freq[n]++;
+            l = min(l, n), h = max(h, n);
+        }
+        int maxF = 0, f = 0, j = l;
+        for(int i = l; i <= h; i++){
+            f += freq[i];
+            while(i - j > 2*k) f -= freq[j++];
+            maxF = max(maxF, f);
+        }
+        return maxF;
+    }
+};
+
 class LessSpaceSolution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
