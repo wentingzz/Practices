8a058b2dbbadb19aba2802c54ba1809dbdacb2cd
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-12-12 16:20:33 -0500

    Update 1268. Search Suggestions System.cpp #1

--- LeetCode/1268. Search Suggestions System.cpp
+++ LeetCode/1268. Search Suggestions System.cpp
@@ -1,3 +1,27 @@
+class SimilarSolution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        sort(products.begin(), products.end());
+        vector<vector<string>> res;
+        int j = 0, n = products.size(), m = searchWord.size();
+        string cur = "";
+        for(int i = 0; i < m; i++){
+            cur += searchWord[i];
+            auto it = lower_bound(products.begin() + j, products.end(), cur);
+            j = distance(it, products.begin());
+            vector<string> list;
+            for(int i = 0; i < 3; i++){
+                if(it == products.end() or it->substr(0, cur.size()) != cur) break;
+                list.push_back(*it);
+                it++;
+            }
+            res.push_back(list);
+        }
+        return res;
+    }
+
+};
+
 class Solution {
 public:
     void addThree(vector<string>& products, string& match, int start, vector<vector<string>>& res){
