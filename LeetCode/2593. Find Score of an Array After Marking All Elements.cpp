e5366959d160ecd28926354f73632b9e21cd80eb
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-12-13 09:48:16 -0500

    Update 2593. Find Score of an Array After Marking All Elements.cpp #1

--- LeetCode/2593. Find Score of an Array After Marking All Elements.cpp
+++ LeetCode/2593. Find Score of an Array After Marking All Elements.cpp
@@ -1,3 +1,26 @@
+class BetterSolution {
+public:
+    long long findScore(vector<int>& nums) {
+        int n = nums.size();
+        vector<pair<int, int>> sorted(n);
+        for(int i = 0; i < n; i++) sorted[i] = {nums[i], i};
+        sort(sorted.begin(), sorted.end());
+        vector<bool> marked(n, false);
+
+        long long res = 0;
+        for(int i = 0; i < n; i++){
+            int idx = sorted[i].second;
+            if(marked[idx]) continue;
+            
+            res += sorted[i].first; 
+            marked[idx] = true;
+            if(idx >= 1) marked[idx-1] = true;
+            if(idx + 1 < n) marked[idx+1] = true;
+        }
+        return res;
+    }
+};
+
 class Solution {
 public:
     long long findScore(vector<int>& nums) {
