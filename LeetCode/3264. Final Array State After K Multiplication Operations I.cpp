99c2ca71315df072752fd40e1e43950ac69f2bc5
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-12-16 15:07:05 -0500

    Update 3264. Final Array State After K Multiplication Operations I.cpp #1

--- LeetCode/3264. Final Array State After K Multiplication Operations I.cpp
+++ LeetCode/3264. Final Array State After K Multiplication Operations I.cpp
@@ -1,3 +1,17 @@
+class QuickerSolution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        for(int i = 0; i < k; i++){
+            int idx = 0;
+            for(int j = 1; j < nums.size(); j++){
+                if(nums[j] < nums[idx]) idx = j;
+            }
+            nums[idx] *= multiplier;
+        }
+        return nums;
+    }
+};
+
 class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
