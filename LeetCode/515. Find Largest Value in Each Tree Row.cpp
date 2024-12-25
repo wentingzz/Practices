5b1d87a6302ff07c1ac9b09d62316df568e990f3
wentingzz <35350631+wentingzz@users.noreply.github.com>
2024-12-24 22:01:36 -0500

    Update 515. Find Largest Value in Each Tree Row.cpp #1

--- LeetCode/515. Find Largest Value in Each Tree Row.cpp
+++ LeetCode/515. Find Largest Value in Each Tree Row.cpp
@@ -27,3 +27,37 @@ public:
         dfs(node->right, res, lvl+1);
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class QuickerSolution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        vector<int> res;
+        queue<TreeNode*> q;
+        if(root) q.push(root);
+        int lvl = 0;
+        while(!q.empty()){
+            int size = q.size();
+            if(res.size() == lvl) res.push_back(INT_MIN);
+            for(int i = 0; i < size; i++){
+                res[lvl] = max(res[lvl], q.front()->val);
+                if(q.front()->left) q.push(q.front()->left);
+                if(q.front()->right) q.push(q.front()->right);
+                q.pop();
+            }
+            lvl++;
+        }
+
+        return res;
+    }
+};
