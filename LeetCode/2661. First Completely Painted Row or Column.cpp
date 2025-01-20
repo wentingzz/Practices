6d6bb4dbbb3deac06a3ff68669e435ad41565d43
wentingzz <35350631+wentingzz@users.noreply.github.com>
2025-01-20 08:36:48 -0500

    Update 2661. First Completely Painted Row or Column.cpp #1

--- LeetCode/2661. First Completely Painted Row or Column.cpp
+++ LeetCode/2661. First Completely Painted Row or Column.cpp
@@ -16,6 +16,24 @@ public:
     }
 };
 
+class SimilarBestSolution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        vector<pair<int, int>> num2pos(m*n+1);
+        vector<int> cols(m, n), rows(n, m);
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++) num2pos[mat[i][j]] = {i, j};
+        }
+        for(int i = 0; i < arr.size(); i++){
+            auto [r,c] = num2pos[arr[i]];
+            rows[c]--, cols[r]--;
+            if(rows[c] == 0 or cols[r] == 0) return i;
+        }
+        return -1;
+    }
+};
+
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
