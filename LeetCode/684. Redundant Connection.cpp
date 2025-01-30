c2be89c981e394a15434b24219235f34ac270858
wentingzz <35350631+wentingzz@users.noreply.github.com>
2025-01-30 09:20:36 -0500

    Update 684. Redundant Connection.cpp #1

--- LeetCode/684. Redundant Connection.cpp
+++ LeetCode/684. Redundant Connection.cpp
@@ -1,4 +1,31 @@
-class BestSpaceSolution {
+class BestSpaceGoodTimeSolution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        vector<int> p(edges.size()+1);
+        for(int i = 0; i < p.size(); i++){
+            p[i] = i;
+        }
+
+        for(auto& edge: edges){
+            int p1 = findP(p, edge[0]);
+            int p2 = findP(p, edge[1]);
+            if(p1 == p2){
+                return edge;
+            }
+            p[p1] = p2;
+        }
+        return {};
+    }
+
+    int findP(vector<int>& p, int node){
+        if(p[node] != node){
+            p[node] = findP(p, p[node]);
+        }
+        return p[node];
+    }
+};
+
+class BestSpaceBadTimeSolution {
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         vector<int> p(edges.size()+1);
