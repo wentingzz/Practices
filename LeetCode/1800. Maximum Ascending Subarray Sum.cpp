958314d560a657c540eb6c2b970236ebec5670fd
wentingzz <35350631+wentingzz@users.noreply.github.com>
2025-02-03 20:18:59 -0500

    Update 1800. Maximum Ascending Subarray Sum.cpp #1

--- LeetCode/1800. Maximum Ascending Subarray Sum.cpp
+++ LeetCode/1800. Maximum Ascending Subarray Sum.cpp
@@ -1,3 +1,18 @@
+class SpaceBetterSolution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int res = 0, cur = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i-1] < nums[i]) cur += nums[i];
+            else{
+                res = max(res, cur);
+                cur = nums[i];
+            }
+            
+        }
+        return max(cur, res);
+    }
+};
 class Solution {
 public:
     int maxAscendingSum(vector<int>& nums) {
