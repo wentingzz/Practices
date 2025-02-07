8c66c30a43487bbea751431ceb063e7547606a78
wentingzz <35350631+wentingzz@users.noreply.github.com>
2025-02-07 08:37:22 -0500

    Update 3160. Find the Number of Distinct Colors Among the Balls.cpp #1

--- LeetCode/3160. Find the Number of Distinct Colors Among the Balls.cpp
+++ LeetCode/3160. Find the Number of Distinct Colors Among the Balls.cpp
@@ -1,4 +1,25 @@
-class Solution {
+class BestSolution {
+public:
+    vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+        unordered_map<int, int> colors, cnt;
+        int n = queries.size(), sum = 0;
+        vector<int> res(n);
+        for(int i = 0; i < n; i++){
+            int idx = queries[i][0], color = queries[i][1];
+            if(colors[idx] != 0){
+                cnt[colors[idx]]--;
+                sum -= (cnt[colors[idx]] == 0);
+            }
+            sum += (cnt[color] == 0);
+            colors[idx] = color;
+            cnt[color]++;
+            res[i] = sum;
+        }
+        return res;
+    }
+};
+
+class MoreSpaceSolution {
 public:
     vector<int> queryResults(int limit, vector<vector<int>>& queries) {
         vector<int> colors(limit + 1, 0);
