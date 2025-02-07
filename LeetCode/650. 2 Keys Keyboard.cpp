8642b1cd4747ffe0576a3e617d05c9554d68a03a
wentingzz <35350631+wentingzz@users.noreply.github.com>
2025-02-07 08:40:06 -0500

    Update 650. 2 Keys Keyboard.cpp #1

--- LeetCode/650. 2 Keys Keyboard.cpp
+++ LeetCode/650. 2 Keys Keyboard.cpp
@@ -1,3 +1,18 @@
+class BestTimeSolution {
+public:
+    int minSteps(int n) {
+        if(n < 2) return 0;
+        vector<int> dp(n+1, 1000);
+        dp[1] = 0;
+        dp[2] = 2;
+        for(int i = 1; i <= n; i++){
+            for(int j = i; j <= n; j += i){
+                dp[j] = min(dp[j], dp[i] + j/i);
+            }
+        }
+        return dp[n];
+    }
+};
 class BestSpaceSolution {
 public:
     int minSteps(int n) {
