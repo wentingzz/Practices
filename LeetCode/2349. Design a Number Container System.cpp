9ea5d86903ee3488a419b79891ef11a4fe9aa1da
wentingzz <35350631+wentingzz@users.noreply.github.com>
2025-02-08 08:48:57 -0500

    Update 2349. Design a Number Container System.cpp #1

--- LeetCode/2349. Design a Number Container System.cpp
+++ LeetCode/2349. Design a Number Container System.cpp
@@ -1,3 +1,35 @@
+class BestNumberContainers {
+public:
+    // map<int, int> m;
+    unordered_map<int, int> i2n;
+    unordered_map<int, priority_queue<int, vector<int>, greater<int>>> n2i;
+    NumberContainers() {
+        
+    }
+    
+    void change(int index, int number) {
+        i2n[index] = number;
+        n2i[number].push(index);
+    }
+    
+    int find(int number) {
+        if(n2i.find(number) == n2i.end()) return -1;
+        while(!n2i[number].empty()){
+            int idx = n2i[number].top();
+            if(i2n[idx] == number) return idx;
+            n2i[number].pop();
+        }
+        return -1;
+    }
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
+
 class SlowerNumberContainers {
 public:
     // map<int, int> m;
