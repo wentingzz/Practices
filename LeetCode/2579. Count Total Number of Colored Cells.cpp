dfc03e30d70180b061776d361f12d3308becbc7e
wentingzz <35350631+wentingzz@users.noreply.github.com>
2025-03-04 20:23:44 -0500

    Update 2579. Count Total Number of Colored Cells.cpp #1

--- LeetCode/2579. Count Total Number of Colored Cells.cpp
+++ LeetCode/2579. Count Total Number of Colored Cells.cpp
@@ -1,3 +1,10 @@
+class BetterSolution {
+public:
+    long long coloredCells(int n) {
+        return 1ll + 4ll * (n-1) * n / 2;
+    }
+};
+
 class Solution {
 public:
     long long coloredCells(int n) {
